7287005c7cb21d5e76b4810eda18942544a35121
Administrator <Administrator@192.168.0.101>
2018-04-04 16:46:51 +0900

    db 서버 설정

--- src/kosta/matchat/model/util/DBProperty.java
+++ src/kosta/matchat/model/util/DBProperty.java
@@ -6,7 +6,7 @@ package kosta.matchat.model.util;
  */
 public interface DBProperty {
 	public static final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
-	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	public static final String USER_NAME = "project";
-	public static final String USER_PASS = "system";
+	public static final String URL = "jdbc:oracle:thin:@192.168.0.196:1521:xe";
+	public static final String USER_NAME = "javaproject";
+	public static final String USER_PASS = "1234";
 }
