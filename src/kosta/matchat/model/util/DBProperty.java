d3c12fd49ebb42e5dc3bff41a0d54a023aa7aa1d
ldy <ldy@192.168.0.107>
2018-04-05 15:30:35 +0900

    다영 : manager 전체list가져오기 추가

--- src/kosta/matchat/model/util/DBProperty.java
+++ src/kosta/matchat/model/util/DBProperty.java
@@ -9,5 +9,5 @@ public interface DBProperty {
 	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
 	//public static final String URL = "jdbc:oracle:thin:@192.168.0.106:1521:xe";
 	public static final String USER_NAME = "project";
-	public static final String USER_PASS = "system";
+	public static final String USER_PASS = "project";
 }
