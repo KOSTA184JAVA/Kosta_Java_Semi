a50b4db3417814ed34d527e79ceca7e10361b71a
LeeWonSeob <leemuo123@gmail.com>
2018-04-05 11:24:25 +0900

    회원가입 뷰 수정

--- src/kosta/matchat/view/start/LoginView.java
+++ src/kosta/matchat/view/start/LoginView.java
@@ -6,7 +6,6 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
